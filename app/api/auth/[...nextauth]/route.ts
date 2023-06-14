31408614599951003f9e33be280ddd45e3c5fb4f
Chahine Benlahcen <chahinebenlahcen@MacBook-Air-de-Chahine.local>
2023-06-14 17:56:33 +0200

    feat: add nextauth and setup

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,56 @@
+import NextAuth from "next-auth/next";
+import githubProvider from "next-auth/providers/github";
+import { prisma } from "@/lib/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const res = await fetch("http://localhost:3000/api/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: credentials?.username,
+            password: credentials?.password,
+          }),
+        });
+
+        const user = await res.json();
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+    githubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+
+    async session({ session, token }) {
+      session.user = token as any;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,24 @@
-import './globals.css'
+import Providers from "@/components/Providers";
+import "./globals.css";
 import { TrpcProvider } from "@/utils/trpc-provider";
 
-
 export const metadata = {
-  title: 'Next13 Trpc Example',
-  description: 'Next13 Trpc Boilerplate',
-}
+  title: "Next13 Trpc Example",
+  description: "Next13 Trpc Boilerplate",
+};
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
-        <TrpcProvider>{children}</TrpcProvider>
+        <TrpcProvider>
+          <Providers>{children}</Providers>
+        </TrpcProvider>
       </body>
     </html>
-  )
+  );
 }
