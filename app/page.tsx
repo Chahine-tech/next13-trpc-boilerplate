451bc2970dc6b42fd614521a0e1611f9c7380d8d
amineohn <amineprojet7@gmail.com>
2023-07-19 09:01:19 +0200

    refactor: stylish indents

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-8">
-      <AppBar />
+        <AppBar />
       </div>
       <ListUsers />
     </main>
